8563b01af6a76c3a0048e809b164185a2a3356f7
WallnutK <jd3762@columbia.edu>
2024-02-22 08:34:56 -0800

    fix: add CartItem, User entities into DB structure

--- API/Data/StoreContext.cs
+++ API/Data/StoreContext.cs
@@ -10,5 +10,19 @@ namespace API.Data
         }
         public DbSet<Product> Products { get; set; }
         public DbSet<Cart> Carts { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<CartItem> Items { get; set; }
+        
+        /*
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // configure entity relationships, primary keys, indexes
+            modelBuilder.Entity<Order>()
+                .HasOne(o => o.User)
+                .WithMany(u => u.Orders)
+                .HasForeignKey(o => o.UserId);
+
+        }
+        */
     }
 }
